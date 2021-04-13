a5c18a902834613aaabc8a6a5c1ccd76169f2248
Gyeonguk-Min <gyunguk94@gmail.com>
2021-04-13 19:38:40 +0900

    storyboard create

---
+++ Shared/example01App.swift
@@ -0,0 +1,17 @@
+//
+//  example01App.swift
+//  Shared
+//
+//  Created by Woogie on 2021/04/13.
+//
+
+import SwiftUI
+
+@main
+struct example01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
